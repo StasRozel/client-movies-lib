d2c37a72c4a4f74924f4f897b951370e93f5574e
Стас <rozelstas@gmail.com>
2023-07-12 01:11:12 +0300

    add file started

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target: 'https://movies-lib-2zxy.onrender.com/',
       changeOrigin: true,
       "secure": false,
       headers: {
